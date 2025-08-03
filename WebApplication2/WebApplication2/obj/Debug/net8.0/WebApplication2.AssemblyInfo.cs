4a48f087a55f62c256a6e77c2bead072756cc582
hamedabutorabi <33637366+hamedabutorabi@users.noreply.github.com>
2025-08-03 23:19:00 +0200

    remove extra project

--- WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc70d09cc019a68f86576170635355930fbfd3dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c621b350cc255c0e74e453260597fe55fdb687")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
